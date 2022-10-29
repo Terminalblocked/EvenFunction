76d44266918775d4f7a738875be273961b25af74
Terminalblocked <mr.pro.simon@mail.ru>
2022-10-29 21:01:14 +0400

    new update

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ function even(a) { // ֆունկցիա որը վերադարձնում է իրե
     if(typeof(a) == "number") { // ստուգում ենք, a - ն թիվ է թե ոչ
         if(a % 2 == 0) { // եթե թիվ է և եթե զույգ է
             alert("Number is even"); // տպում ենք որ զույգ է
+            console.log("There are several other even numbers before this number " + a); // հիմա տպում ենք թե a - ից առաջ քանի զույգ թիվ կա
+            for(let i = 1; i < a; i++) {
+                if(i % 2 == 0) {
+                    console.log(i);
+                }
+            }
         } else { // եթե թիվ է բայց զույգ չէ
             alert("Number is odd"); // տպում ենք որ կենտ է
+            console.log("There are several other odd numbers before number " + a); // հիմա տպում ենք թե a - ից առաջ քանի կենտ թիվ կա
+            for(let i = 1; i < a; i++) {
+                if(i % 2 !== 0) {
+                    console.log(i);
+                }
+            }
         }
     } else { // եթե թիվ չէ հակառակ դեպքում
         alert("Not a number!"); // տպում ենք որ թիվ չէ
